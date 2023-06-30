69f633842898e3dbf27663257c252b5f83c50669
Bruno Rodrigues <bytebruno@gmail.com>
2023-06-30 18:38:09 +0100

    chore: add ng-zorro

--- apps/customer/src/app/app.module.ts
+++ apps/customer/src/app/app.module.ts
@@ -6,6 +6,15 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 import { AppRoutingModule } from './app.routing.module';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -18,8 +27,13 @@ import { AppRoutingModule } from './app.routing.module';
       logOnly: !isDevMode(),
     }),
     EffectsModule.forRoot(),
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
