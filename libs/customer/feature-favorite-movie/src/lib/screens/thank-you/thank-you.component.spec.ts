0b043846a15cbca668802606184e2d71dcb23c9a
Bruno Rodrigues <bytebruno@gmail.com>
2023-07-03 17:24:12 +0100

    test: thank-you component

--- libs/customer/feature-favorite-movie/src/lib/screens/thank-you/thank-you.component.spec.ts
+++ libs/customer/feature-favorite-movie/src/lib/screens/thank-you/thank-you.component.spec.ts
@@ -1,13 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ThankYouComponent } from './thank-you.component';
+import { of } from 'rxjs';
+import { CustomerData } from '@angular-nx-ddd/customer/domain';
+import { SharedUiCommonModule } from '@angular-nx-ddd/shared/ui-common';
+import { By } from '@angular/platform-browser';
 
-describe('ThankYouComponent', () => {
+describe.only('ThankYouComponent', () => {
   let component: ThankYouComponent;
   let fixture: ComponentFixture<ThankYouComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ThankYouComponent],
+      imports: [SharedUiCommonModule],
+      declarations: [ThankYouComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ThankYouComponent);
@@ -17,5 +22,27 @@ describe('ThankYouComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show card only if has customer data', () => {
+    component.customerData$ = of<CustomerData>({
+      name: 'John',
+      userName: 'john@email.com',
+      country: 'Ireland',
+      postCode: '123 21',
+      favoriteMovie: 'LOTR',
+    });
+
+    fixture.detectChanges();
+
+    const userCard = fixture.debugElement.query(By.css('.customer-data-card'));
+    expect(userCard).not.toBeNull();
+  });
+
+  it('should hide card only if has no customer data', () => {
+    component.customerData$ = of(null);
+    fixture.detectChanges();
+    const userCard = fixture.debugElement.query(By.css('.customer-data-card'));
+    expect(userCard).toBeNull();
   });
 });
