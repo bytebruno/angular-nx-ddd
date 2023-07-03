3c2ea8d4696697d5b5286bd7173521dc29dcd86d
Bruno Rodrigues <bytebruno@gmail.com>
2023-07-03 18:09:19 +0100

    test: default tests on app and country-selection

--- libs/shared/ui-common/src/lib/components/country-selection/country-selection.component.spec.ts
+++ libs/shared/ui-common/src/lib/components/country-selection/country-selection.component.spec.ts
@@ -1,5 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CountrySelectionComponent } from './country-selection.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Country } from '@angular-nx-ddd/shared/util-country';
+import { NgZorroModule } from '../../ng-zorro/ng-zorro.module';
+import { CommonModule } from '@angular/common';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('CountrySelectionComponent', () => {
   let component: CountrySelectionComponent;
@@ -7,11 +13,25 @@ describe('CountrySelectionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        NgZorroModule,
+        ReactiveFormsModule,
+        NoopAnimationsModule,
+        HttpClientTestingModule,
+      ],
       declarations: [CountrySelectionComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CountrySelectionComponent);
     component = fixture.componentInstance;
+
+    component.parentForm = new FormGroup({
+      country: new FormControl<Country | null>(null),
+    });
+
+    component.controlName = 'country';
+
     fixture.detectChanges();
   });
 
